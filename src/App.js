e9f5af76bd65abb3b94ed5bd1f33650ee2d2ed13
harika09 <harold.goldensky.com>
2021-06-05 21:24:55 +0800

    Initial build

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import Sedans from './Images/icon-sedans.svg'
+import Suvs from './Images/icon-suvs.svg'
+import Luxury from './Images/icon-luxury.svg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main-container'>
+      <div className='main-wrapper bd-container'>
+
+        <div className='card sedans'>
+          <img src={Sedans} alt="" />
+            <h2>SEDANS</h2>
+
+            <p>Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city 
+            or on your next road trip.</p>
+
+            <button className='btn-learn-more'>
+              Learn More
+            </button>
+        </div>
+
+        <div className='card suv'>
+          <img src={Suvs} alt="" />
+            <h2>SUVs</h2>
+
+            <p>Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury 
+            rental and arrive in style.</p>
+
+            <button className='btn-learn-more'>
+              Learn More
+            </button>
+        </div>
+
+        <div className='card'>
+          <img src={Luxury} alt="" />
+            <h2>Luxury</h2>
+
+            <p>Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury 
+            rental and arrive in style.</p>
+
+            <button className='btn-learn-more'>
+              Learn More
+            </button>
+        </div>
+
+      </div>
     </div>
   );
 }
